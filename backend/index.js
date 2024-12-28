49dbd8221f016b2eb14a6314d0e98963a3bcaa5f
Vidit Pandey <viditpandey06@gmail.com>
2024-12-28 14:31:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'true',
     credentials:true
 }
 
