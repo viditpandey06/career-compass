3df8072d7d05ede4d83b985b6efa4fd8c83c5858
Vidit Pandey <viditpandey06@gmail.com>
2024-12-28 15:40:27 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://career-compass-web-app.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://career-compass-web-app.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://career-compass-web-app.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://career-compass-web-app.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://jobportal.viditpandey.tech/api/v1/user";
+export const JOB_API_END_POINT="https://jobportal.viditpandey.tech/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobportal.viditpandey.tech/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobportal.viditpandey.tech/api/v1/company";
